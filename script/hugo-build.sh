cd18296f92372ec732984363e5b8fece15cb5504
c332030 <c332030@gmail.com>
2022-07-10 20:54:52 +0800

    feat: hugo-build.sh

---
+++ script/hugo-build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+SOURCE_REPO=$1
+BUILD_REPO=$2
+
+echo "SOURCE=$SOURCE_REPO"
+echo "BUILD=$BUILD_REPO"
+
+git clone --recursive "${SOURCE_REPO}" source && cd source
+
+git clone --depth=1 "${BUILD_REPO}" public-pages
+
+mkdir public
+mv public-pages/.git public/
+
+hugo && cd public
+
+git add -A
+git commit -m "auto build by cc332030/hugo-asciidoctor-action" || exit 0
+git push
