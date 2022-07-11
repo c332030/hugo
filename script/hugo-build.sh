14515335c8e392d303bfbb4f8b87189556faa65e
c332030 <c332030@gmail.com>
2022-07-11 21:13:31 +0800

    feat: COMMAND

--- script/hugo-build.sh
+++ script/hugo-build.sh
@@ -23,4 +23,9 @@ git add -A
 git commit -m "auto build by cc332030/hugo-asciidoctor-action" || exit 0
 git push
 
+if [ -n "$COMMAND" ]
+then
+  ${COMMAND} || true
+fi
+
 echo 'hugo-build successfully'
